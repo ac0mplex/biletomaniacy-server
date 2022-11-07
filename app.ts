252a56b31dc56932c48a17341fd87a3e5a8b500b
acomplex <bartlomiej345@gmail.com>
2022-11-07 20:18:40 +0100

    Model bazy danych i przykładowe query

--- app.ts
+++ app.ts
@@ -1,10 +1,26 @@
-import express, { Request, Response } from 'express';
+// TODO: github.com/tsconfig/bases
+
+import express from 'express';
+import pg from 'pg';
 
 const app = express();
 const port = 3019;
 
-app.get('/', (_request: Request, response: Response) => {
-	response.send('Hello World!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new pg.Pool({
+	database: 'biletomaniacy'
+});
+
+app.get('/', async (_request, response, next) => {
+	pool.query('SELECT name FROM "user"', (error, results) => {
+		if (error) {
+			next(error);
+		} else {
+			response.json({ data: results.rows });
+		}
+	});
 });
 
 app.listen(port, () => {
---
+++ model.sql
@@ -0,0 +1,77 @@
+-- Database generated with pgModeler (PostgreSQL Database Modeler).
+-- pgModeler version: 1.0.0-beta
+-- PostgreSQL version: 15.0
+-- Project Site: pgmodeler.io
+-- Model Author: ---
+
+-- Database creation must be performed outside a multi lined SQL file. 
+-- These commands were put in this file only as a convenience.
+-- 
+-- object: biletomaniacy | type: DATABASE --
+-- DROP DATABASE IF EXISTS biletomaniacy;
+CREATE DATABASE biletomaniacy
+	ENCODING = 'UTF8'
+	LC_COLLATE = 'en_US.UTF-8'
+	LC_CTYPE = 'en_US.UTF-8';
+-- ddl-end --
+
+
+-- object: public."user" | type: TABLE --
+-- DROP TABLE IF EXISTS public."user";
+CREATE TABLE public."user" (
+	id serial NOT NULL,
+	name text NOT NULL,
+	password text NOT NULL,
+	admin boolean NOT NULL DEFAULT False,
+	CONSTRAINT user_pk PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
+-- ddl-end --
+ALTER TABLE public."user" OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.concert | type: TABLE --
+-- DROP TABLE IF EXISTS public.concert;
+CREATE TABLE public.concert (
+	id serial NOT NULL,
+	name text NOT NULL,
+	location text,
+	date timestamp NOT NULL,
+	CONSTRAINT concert_pk PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
+-- ddl-end --
+ALTER TABLE public.concert OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.ticket | type: TABLE --
+-- DROP TABLE IF EXISTS public.ticket;
+CREATE TABLE public.ticket (
+	id serial NOT NULL,
+	"row" integer,
+	"column" integer,
+	payed boolean NOT NULL DEFAULT false,
+	concert_id integer NOT NULL,
+	user_id integer,
+	CONSTRAINT ticket_pk PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
+-- ddl-end --
+ALTER TABLE public.ticket OWNER TO postgres;
+-- ddl-end --
+
+-- object: concert_fk | type: CONSTRAINT --
+-- ALTER TABLE public.ticket DROP CONSTRAINT IF EXISTS concert_fk CASCADE;
+ALTER TABLE public.ticket ADD CONSTRAINT concert_fk FOREIGN KEY (concert_id)
+REFERENCES public.concert (id) MATCH SIMPLE
+ON DELETE CASCADE ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: user_fk | type: CONSTRAINT --
+-- ALTER TABLE public.ticket DROP CONSTRAINT IF EXISTS user_fk CASCADE;
+ALTER TABLE public.ticket ADD CONSTRAINT user_fk FOREIGN KEY (user_id)
+REFERENCES public."user" (id) MATCH SIMPLE
+ON DELETE SET NULL ON UPDATE NO ACTION;
+-- ddl-end --
+
+
